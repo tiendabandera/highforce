b4aef108958832814c40b32036e87007a605c394
libardoValera <161627329+libardoValera@users.noreply.github.com>
2024-02-28 12:54:39 -0500

    Add files via upload

---
+++ store.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Ya estamos cambiando.....');
+    window.Ecwid.OnPageLoaded.add(function (page) {        
+        function execute(params) {             
+            var section = document.getElementById("section-guia-talla");
+            var paragraph = document.createElement("p");
+            var textNode = document.createTextNode("Yo estoy aquí");
+            
+            paragraph.appendChild(textNode);                   
+            section.appendChild(paragraph);
+            
+            var button = document.createElement("button");                    
+            var buttonText = document.createTextNode("Presiona aquí");
+           
+            button.appendChild(buttonText);
+            
+            button.onclick = function () {
+                alert("aca estoy");
+            };
+            
+            section.appendChild(button);
+        }
+        execute();
+    });
+});
