2d6a3041e0dab629c3c08ad331edbce0d570e38a
libardoValera <161627329+libardoValera@users.noreply.github.com>
2024-02-28 12:57:59 -0500

    Update store.js

--- store.js
+++ store.js
@@ -2,22 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Ya estamos cambiando.....');
     window.Ecwid.OnPageLoaded.add(function (page) {        
         function execute(params) {             
-            var section = document.getElementById("section-guia-talla");
-            var paragraph = document.createElement("p");
-            var textNode = document.createTextNode("Yo estoy aquí");
-            
-            paragraph.appendChild(textNode);                   
-            section.appendChild(paragraph);
-            
+            var section = document.getElementById("section-guia-talla");                
             var button = document.createElement("button");                    
             var buttonText = document.createTextNode("Presiona aquí");
-           
-            button.appendChild(buttonText);
             
+            button.appendChild(buttonText);            
             button.onclick = function () {
                 alert("aca estoy");
-            };
-            
+            };            
             section.appendChild(button);
         }
         execute();
