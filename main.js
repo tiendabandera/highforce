39e19d96826b69fe4d63de66ed83334840ed1d64
libardoValera <161627329+libardoValera@users.noreply.github.com>
2024-02-28 12:12:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-window.Ecwid.OnPageLoaded.add(function (page) {
-                
-    function execute(params) {             
-        var section = document.getElementById("section-guia-talla");
-        var paragraph = document.createElement("p");
-        var textNode = document.createTextNode("Yo estoy aquí");
-        
-        paragraph.appendChild(textNode);                   
-        section.appendChild(paragraph);
-        
-        var button = document.createElement("button");                    
-        var buttonText = document.createTextNode("Presiona aquí");
-       
-        button.appendChild(buttonText);
-        
-        button.onclick = function () {
-            alert("aca estoy");
-        };
-        
-        section.appendChild(button);
-    }
-    execute();
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Proximamente.....');
 });
